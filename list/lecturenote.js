562b2cf70b5f00d1204913bce8beecc98c11f875
Sparisoma Viridi <dudung@gmail.com>
2017-08-21 22:38:16 +0900

    Update lecturenote.js

--- list/lecturenote.js
+++ list/lecturenote.js
@@ -36,3 +36,53 @@ var p = new LectureNote(
 	"http://dx.doi.org/10.13140/RG.2.2.34734.31047"
 );
 pubs.push(p);
+
+var p = new LectureNote(
+	"S. Viridi",
+	"Komputasi Fisika",
+	"Prodi Fisika, FMIPA, Institut Teknologi Bandung",
+	"Dec",
+	"2016",
+	"http://dx.doi.org/10.13140/RG.2.2.21787.87844"
+);
+pubs.push(p);
+
+var p = new LectureNote(
+	"S. Viridi, S. N. Khotimah, Suprijadi",
+	"Komputasi Fisika 2: Simulasi dan Visualisasi Fisika dengan C++ dan Gnuplot",
+	"May",
+	"2016",
+	"Departemen Fisika, FMIPA, Institut Teknologi Bandung",
+	"http://dx.doi.org/10.13140/RG.2.2.31633.22880"
+);
+pubs.push(p);
+
+var p = new LectureNote(
+	"S. Viridi, Suprijadi",
+	"Komputasi Fisika 1: Simulasi dan Visualisasi Fisika dengan C, C++, Gnuplot, dan OpenGL",
+	"Departemen Fisika, FMIPA, Institut Teknologi Bandung",
+	"Dec",
+	"2015",
+	"http://dx.doi.org/"
+);
+pubs.push(p);
+
+var p = new LectureNote(
+	"S. Viridi",
+	"Fisika Dasar",
+	"Prodi Fisika, FMIPA, ITB",
+	"May",
+	"2011",
+	"http://dx.doi.org/10.13140/RG.2.1.2536.2801"
+);
+pubs.push(p);
+
+var p = new LectureNote(
+	"S. Viridi, S. N. Khotimah, Novitrian",
+	"Catatan Kuliah Fisika Statistik",
+	"Departemen Fisika, FMIPA, Institut Teknologi Bandung",
+	"Aug",
+	"2010",
+	"http://dx.doi.org/10.13140/RG.2.1.4588.5287"
+);
+pubs.push(p);
