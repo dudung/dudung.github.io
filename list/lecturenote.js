70ea2421a81a0b071eb7088dbb38a2056b1e50bc
Sparisoma Viridi <dudung@gmail.com>
2017-08-21 22:22:34 +0900

    Add files via upload

---
+++ list/lecturenote.js
@@ -0,0 +1,38 @@
+// ------------------------------------
+// Lecture Notes
+// ------------------------------------
+
+// 20170821 at Osaka University International House Room 204
+
+//var LectureNote = createBibType("author|title|publisher|version|year|url");
+
+
+var p = new LectureNote(
+	"S. Viridi, P. Wulandari, S. Komalasari",
+	"Soal(+Jawab):FI1202-01@2016-3",
+	"Prodi Fisika, Institut Teknologi Bandung",
+	"Aug",
+	"2017",
+	"http://dx.doi.org/10.13140/RG.2.2.18629.55523"
+);
+pubs.push(p);
+
+var p = new LectureNote(
+	"S. Viridi, R. Kurniadi",
+	"Komputasi Fisika 3: Simulasi dan Visualisasi Fisika dengan JavaScript",
+	"Departemen Fisika, FMIPA, Institut Teknologi Bandung",
+	"May",
+	"2017",
+	"http://dx.doi.org/10.13140/RG.2.2.35065.67680"
+);
+pubs.push(p);
+
+var p = new LectureNote(
+	"S. Viridi",
+	"Workshop Peningkatan Profesionalitas Guru Pembina Olimpiade Sains: Fisika",
+	"LPP Salman ITB",
+	"Feb",
+	"2017",
+	"http://dx.doi.org/10.13140/RG.2.2.34734.31047"
+);
+pubs.push(p);
