3bfc3201d85793e138d7075858aec427a32fe17a
Sparisoma Viridi <dudung@gmail.com>
2017-12-13 07:15:01 +0700

    Add files via upload

---
+++ list/inarxiv.js
@@ -0,0 +1,82 @@
+// ------------------------------------
+// INA-Rxiv
+// ------------------------------------
+
+var p = new INARxiv(
+	"S. Viridi, F. Haryanto, F. Purnama",
+	"Teach Yourself Fluid Concepts: How to Use Internet Resources",
+	"31 Oct",
+	"2017",
+	"http://dx.doi.org/10.17605/OSF.IO/85SAF"
+);
+pubs.push(p);
+
+var p = new viXra(
+	"S. Viridi, Y. Bindar, S. Suhandono, E. Kardena, Rifqi, A. E. Fitriani, S. I. Ihsani",
+	"Use of Moore’s Law to Predict Implementation and Advancing of Science and Technology in Supporting Halal Requirements: a Scopus Study",
+	"1509.0271",
+	"histph",
+	"09-29",
+	"2015"
+);
+pubs.push(p);
+
+var p = new viXra(
+	"S. Viridi, T. W. Purboyo",
+	"Implementation of STL Vector Class in Reproducing Results from 'An Improved Algorithm for Generation of Attack Graph Based on Virtual Performance Node' Through Random ip Determination",
+	"1508.0186",
+	"dsalg",
+	"08-23",
+	"2015"
+);
+pubs.push(p);
+
+var p = new viXra(
+	"S. Viridi, S. N. Khotimah, Yulia, Y. Mardiansyah",
+	"Point Mass Model for Predicting 2-D Particles Motion Modes in Vertical Rotation Drum",
+	"1508.0126",
+	"condmt",
+	"09-16",
+	"2015"
+);
+pubs.push(p);
+
+var p = new viXra(
+	"S. Viridi, A. Suroso, F. T. Akbar, Novitrian, T. D. K. Wungu, S. Pramuditya, D. Irwanto, N. Asiah, A. Pramutadi, K. Basar, F. D. E. Latief, S. Permana, I. D. Aditya, H. Mahardika, A. H. Aimon, A. Waris, Khairurrijal",
+	"A (Bash) Shell Script for Creating LATEX-based Book of Abstract in Supporting SeminarPress Application Version 1.31 (July 2015)",
+	"1507.0080",
+	"dsalg",
+	"07-12",
+	"2015"
+);
+pubs.push(p);
+
+var p = new viXra(
+	"S. Viridi, Widayani, S. N. Khotimah",
+	"Elastisitas Batang Lurus Komposit Biner Dengan Model Pegas-Titik Massa Dalam Serat Lurus Sejajar Tak Berinteraksi: Studi Awal Kasus Serat Tunggal",
+	"1506.0078",
+	"condmt",
+	"06-10",
+	"2015"
+);
+pubs.push(p);
+
+var p = new viXra(
+	"S. Viridi, W. Yusman, S. Rachmat",
+	"Implementation of Hagedorn-Brown Correlation Method in Single Fluid Volume Element (SFVE) Method for Predicting Flow of Geothermal Well based on Pressure-Depth and Temperature-Depth Measurement Data at Static Condition",
+	"1506.0010",
+	"geop",
+	"06-02",
+	"2015"
+);
+pubs.push(p);
+
+var p = new viXra(
+	"S. Viridi, Suprijadi, R. R. Septiawan",
+	"Formulation of Porosity Calculation for Three-Dimension Granular Materials in the Case of Spherical Particles",
+	"1503.0188",
+	"condmt",
+	"03-26",
+	"2015"
+);
+pubs.push(p);
