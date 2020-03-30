2c644b28444506296da29b50646ebf84f9a832a8
dudung <dudung@gmail.com>
2020-03-30 19:10:02 +0700

    mod zenodo

--- list/zenodo.js
+++ list/zenodo.js
@@ -3,12 +3,41 @@
 // ------------------------------------
 
 
-// Add on 20200321.008
+// Add on 20200330.1905
+var p = new Zenodo(
+	"S. Viridi, N. Nuraini, I. Taufik, A. Susandi, P. Aditiawati",
+	"Discussion #2 about SIR Model: Intra-area and Inter-area Components Relation",
+	"3732836",
+	"30 Mar",
+	"2020"
+);
+pubs.push(p);
+
+var p = new Zenodo(
+	"S. Viridi, R. Kurniadi",
+	"40 | Pengantar Singkat Contoh Penerapan Metode Dinamika Molekuler pada Beberapa Kasus Non-Molekuler – II",
+	"3731975",
+	"28 Mar",
+	"2020"
+);
+pubs.push(p);
+
+var p = new Zenodo(
+	"S. Viridi, R. Kurniadi",
+	"40 | Pengantar Singkat Contoh Penerapan Metode Dinamika Molekuler pada Beberapa Kasus Non-Molekuler – I",
+	"3731160",
+	"27 Mar",
+	"2020"
+);
+pubs.push(p);
+
+
+// Add on 20200321.0008
 var p = new Zenodo(
 	"S. Viridi, I. Taufik, A. Susandi, P. Aditiawati",
 	"Discussion #1 about SIR Model: Introducing Spatial Aspect",
 	"3719365",
-	"3 Mar",
+	"20 Mar",
 	"2020"
 );
 pubs.push(p);
